a395cae637ee63111fc779b12020931e82ce5c4f
chfle <christian.lehnert.home@gmail.com>
2023-03-09 05:24:57 -0500

    remember me and basic web security and basic side panel

--- src/main/java/org/lehnert/todo/config/WebServerConfig.java
+++ src/main/java/org/lehnert/todo/config/WebServerConfig.java
@@ -2,21 +2,21 @@ package org.lehnert.todo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.stereotype.Controller;
 
 @EnableWebSecurity
 @Configuration
 public class WebServerConfig  {
    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-       http.httpBasic()
-               .and().formLogin()
-               .and().rememberMe();
+       http.csrf();
+       http.cors();
+
+       http.authorizeHttpRequests().anyRequest().authenticated();
+
+       http.httpBasic().and().formLogin().and().rememberMe();
 
        return http.build();
    }
