f633ca4b0864af8f8306240836827093ea0c3a98
chfle <christian.lehnert.home@gmail.com>
2023-03-14 14:16:04 +0100

    basic page layout

---
+++ src/main/java/org/lehnert/todo/controller/ErrorController.java
@@ -0,0 +1,14 @@
+package org.lehnert.todo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/error")
+public class ErrorController {
+    @GetMapping
+    String getErrorPage() {
+        return "error";
+    }
+}
