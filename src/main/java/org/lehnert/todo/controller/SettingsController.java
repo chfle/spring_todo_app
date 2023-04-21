5ea4c3f6d4f17cca348b3692069ff9b406d3b297
chfle <christian.lehnert.home@gmail.com>
2023-04-21 07:15:51 -0400

    delete account page

--- src/main/java/org/lehnert/todo/controller/SettingsController.java
+++ src/main/java/org/lehnert/todo/controller/SettingsController.java
@@ -1,6 +1,8 @@
 package org.lehnert.todo.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/settings")
 public class SettingsController {
     @GetMapping
-    String getSettings() {
-        return "settings";
+    String getSettings(Model model, Authentication authentication) {
+        model.addAttribute("username", authentication.getName());
+        return "settings/settings";
+    }
+
+    @GetMapping("/delete")
+    String getDeleteSetting(Model model, Authentication authentication) {
+        model.addAttribute("username", authentication.getName());
+        return "/settings/delete";
     }
 }
